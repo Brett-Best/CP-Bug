0a82353a66d95be66c15e6bd2c61a83e20aa47af
Brett Best <brettbest@live.com.au>
2020-01-08 16:11:32 +1100

    Initial Commit

---
+++ CP-Bug/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CP-Bug
+//
+//  Created by Brett Best on 8/1/20.
+//  Copyright © 2020 Brett Best. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
